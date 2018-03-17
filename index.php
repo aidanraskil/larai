9f75bcea0c1a764f9b632abaa42fe149e08418dd
aidanraskil <github@aidanraskil.bulc.club>
2018-03-17 10:38:20 +0800

    Here we go

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Larai</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Selamat Datang</h1>
+		<?php
+			phpinfo();
+		?>
+	</div>
+</body>
+</html>
