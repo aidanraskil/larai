4113588ef55b4d432d4b49c350ead26fd03ae76e
aidanraskil <github@aidanraskil.bulc.club>
2018-03-19 22:47:57 +0800

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
                     <h1>Welcome To Larai</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae similique architecto, assumenda maxime ipsa. Dolor id tenetur temporibus fugiat eos labore ipsam sed tempora nihil. Iusto officia unde ducimus fugiat.</p>
                 </div>
             </div>
         </div>
