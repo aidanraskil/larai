79b37d0384b66c21ba2818cf86c0d8840f38b813
aidanraskil <github@aidanraskil.bulc.club>
2018-03-19 23:44:07 +0800

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-body">
                     <h1>Welcome To Larai</h1>
                     <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae similique architecto, assumenda maxime ipsa. Dolor id tenetur temporibus fugiat eos labore ipsam sed tempora nihil. Iusto officia unde ducimus fugiat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, molestias necessitatibus vel est commodi ipsa incidunt, natus perspiciatis dicta cupiditate iusto rerum voluptatum, magnam modi vero inventore nihil suscipit sapiente. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse dignissimos debitis, praesentium odit eos accusantium velit repudiandae similique. At animi ex praesentium qui libero consequatur dolorem, nobis suscipit odio deserunt!</p>
                 </div>
             </div>
         </div>
